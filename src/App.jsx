24a0db49a9041880003264ace86ab916035324ce
njonusiene <jonusiene.n@gmail.com>
2023-12-28 11:42:41 +0200

    praktika

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Button from './components/button'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header>
+      <Button color={'white'} text={'White'}/>
+      <Button color={'black'} text={'Black'}/>
+      <Button color={'black'} text={'Hello'}/>      
+    </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.jsx
@@ -0,0 +1,5 @@
+const Button = ({color, text}) => {
+    return <button className={color}>{text}</button>
+  }
+
+  export default Button
